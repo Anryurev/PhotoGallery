25212cff4bdf472b14b5a6d7895fe09b85cb2178
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-08 22:11:47 +0700

    Добавление зависимостей Gson (app/build.gradle). Переопределение сопоставления имен-свойств по умолчанию (GalleryItem.kt). Добавление PhotoResponse (PhotoResponse.kt). Добавление FlickrResponse (FlickrResponse.kt). Изменение возвращаемого типа fetchPhoto() (api/FlickrApi.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
@@ -1,3 +1,5 @@
 package com.bignerdranch.android.photogallery
 
-data class GalleryItem(var title: String = "", var id: String = "", var url: String = "")
+import com.google.gson.annotations.SerializedName
+
+data class GalleryItem(var title: String = "", var id: String = "", @SerializedName("url_s") var url: String = "")
--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,9 +1,10 @@
 package com.bignerdranch.android.photogallery.api
 
+import com.bignerdranch.android.photogallery.FlickrResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
     @GET("services/rest/? method=flickr.interestingness.getList" + "&api_key=e84a44be958b350c8aaaccea7d2c3f8e" + "&format=json" + "&nojsoncallback=1" + "&extras=url_s")
-    fun fetchPhotos(): Call<String>
+    fun fetchPhotos(): Call<FlickrResponse>
 }
