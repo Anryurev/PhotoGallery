8d7423da71877034b24654d061fa60c73acbe14e
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-17 17:44:11 +0700

    Добавление перехватчика для вставки URL-констант (api/PhotoInterceptor.kt). Добавление перехватчика в конфигурацию Retrofit (FlickrFetchr.kt). Добавление функции поиска во FlickrApi (api/FlickrApi.kt). Добавление функции поиска во FlickrFetchr (FlickrFetchr.kt).  Запуск поискового запроса (PhotoGalleryViewModel.kt).

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,11 +4,15 @@ import com.bignerdranch.android.photogallery.FlickrResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET("services/rest/? method=flickr.interestingness.getList" + "&api_key=e84a44be958b350c8aaaccea7d2c3f8e" + "&format=json" + "&nojsoncallback=1" + "&extras=url_s")
+    @GET("services/rest? method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+    @GET("services/rest? method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
+
 }
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/PhotoInterceptor.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.photogallery.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "e84a44be958b350c8aaaccea7d2c3f8e"
+class PhotoInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+        val newUrl: HttpUrl = originalRequest.url().newBuilder().addQueryParameter("api_key", API_KEY).addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallb ack", "1").addQueryParameter("extras", "url_s").addQueryParameter("safesearch", "1").build()
+        val newRequest: Request = originalRequest.newBuilder().url(newUrl).build()
+        return chain.proceed(newRequest)
+    }
+}
