0d138d94a8a1d970b4b08acb552177dc3efa2189
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-17 22:28:37 +0700

    Сохранение состояния Worker (QueryPreferences.kt). Добавление ресурсов переключения опроса (res/values/strings.xml). Добавление переключения между опросами (res/menu/fragment_photo_gallery.xml). Установка правильного текста пункта меню (PhotoGalleryFragment.kt). Обработка кликов по опросам (PhotoGalleryFragment.kt).

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -13,27 +13,22 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
-import com.bignerdranch.android.photogallery.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "PhotoGalleryFragment"
+private const val POLL_WORK = "POLL_WORK"
 class PhotoGalleryFragment : Fragment(){
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
@@ -48,10 +43,7 @@ class PhotoGalleryFragment : Fragment(){
                 val drawable = BitmapDrawable(resources, bitmap)
                 photoHolder.bindDrawable(drawable)
         }
-        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
-        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED).build()
-        val workRequest = OneTimeWorkRequest.Builder(PollWorker::class.java).setConstraints(constraints).build()
-        WorkManager.getInstance().enqueue(workRequest)
+        lifecycle.addObserver(thumbnailDownloader)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -98,8 +90,15 @@ class PhotoGalleryFragment : Fragment(){
             setOnSearchClickListener {
                 searchView.setQuery(photoGalleryViewModel.searchTerm, false)
             }
-
         }
+        val toggleItem = menu.findItem(R.id.menu_item_toggle_polling)
+        val isPolling = QueryPreferences.isPolling(requireContext())
+        val toggleItemTitle = if (isPolling) {
+            R.string.stop_polling
+        } else {
+            R.string.start_polling
+        }
+        toggleItem.setTitle(toggleItemTitle)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -107,6 +106,21 @@ class PhotoGalleryFragment : Fragment(){
                 photoGalleryViewModel.fetchPhotos("")
                 true
             }
+            R.id.menu_item_toggle_polling -> {
+                val isPolling = QueryPreferences.isPolling(requireContext())
+                if (isPolling) {
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(), false)
+                } else {
+                    val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED).build()
+                    val periodicRequest = PeriodicWorkRequest.Builder(PollWorker::class.java, 15, TimeUnit.MINUTES).setConstraints(constraints).build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(POLL_WORK, ExistingPeriodicWorkPolicy.KEEP, periodicRequest)
+                    QueryPreferences.setPolling(requireContext(), true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
