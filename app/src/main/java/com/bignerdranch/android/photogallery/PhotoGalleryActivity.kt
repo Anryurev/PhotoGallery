afcbda123da50af89ee436f070eea231e781f3bb
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-17 21:57:51 +0700

    Создание канала уведомлений (PhotoGalleryApplication.kt). Добавление строк (res/values/strings.xml). Обновление тега приложения в манифесте (manifests/AndroidManifest.xml). Добавление newIntent(...) в PhotoGalleryActivity (PhotoGalleryActivity.kt). Добавление уведомления (PollWorker.kt).

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,8 +11,13 @@ class PhotoGalleryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo_gallery)
         val isFragmentContainerEmpty = savedInstanceState == null
         if (isFragmentContainerEmpty) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
+        }
+    }
+    companion object {
+        fun newIntent(context: Context): Intent
+        {
+            return Intent(context, PhotoGalleryActivity::class.java)
         }
     }
 }
