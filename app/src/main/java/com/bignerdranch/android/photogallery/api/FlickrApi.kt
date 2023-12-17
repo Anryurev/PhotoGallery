e503f5612e9afe2ac0a80721c72aabc2761f76ba
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-12-17 14:25:22 +0700

    Изменение кода FlickrApi (api/FlickrApi.kt). Добавление загрузки изображения во FlickrFetchr (FlickrFetchr.kt).

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.photogallery.api
 
 import com.bignerdranch.android.photogallery.FlickrResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET("services/rest/? method=flickr.interestingness.getList" + "&api_key=e84a44be958b350c8aaaccea7d2c3f8e" + "&format=json" + "&nojsoncallback=1" + "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
